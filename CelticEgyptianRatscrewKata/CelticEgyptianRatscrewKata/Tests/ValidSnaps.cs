c07e296e1103b30f181a5874ecefff3147d7a1d7
Alastair Smith <github@alastairsmith.me.uk>
2015-01-23 15:19:32 +0000

    Second test: no match on single-card stack.

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/ValidSnaps.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/ValidSnaps.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace CelticEgyptianRatscrewKata.Tests
@@ -24,9 +25,20 @@ namespace CelticEgyptianRatscrewKata.Tests
 				Assert.That(matches);
 			}
 
+			[Test]
+			public void Should_Not_Match_For_A_Single_Card_Stack()
+			{
+				var stack = new Stack(new List<Card> { new Card(Suit.Clubs, Rank.Ace) });
+
+				var standardSnap = new StandardSnap();
+				var theSnap = standardSnap.IsValidFor(stack);
+
+				Assert.That(theSnap, Is.False);
+			}
+
 			private bool IsValidFor(Stack stack)
 			{
-				return true;
+				return stack.Count() != 1;
 			}
 		}
 	}
