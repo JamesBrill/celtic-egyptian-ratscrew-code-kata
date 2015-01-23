efa7aceb14f8869c761710bb36292867d7f18428
Alastair Smith <github@alastairsmith.me.uk>
2015-01-23 14:58:05 +0000

    First test: matches two consecutive cards.
    Noddy catch-all implementation.

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/ValidSnaps.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+	class ValidSnapsIncludes
+	{
+		[TestFixture]
+		class StandardSnap
+		{
+			[Test]
+			public void Should_Match_Two_Consecutive_Cards_Of_The_Same_Rank()
+			{
+				var cards = new List<Card>
+				{
+					new Card(Suit.Clubs, Rank.Ace),
+					new Card(Suit.Diamonds, Rank.Ace)
+				};
+				var stack = new Stack(cards);
+
+				var standardSnap = new StandardSnap();
+				var matches = standardSnap.IsValidFor(stack);
+
+				Assert.That(matches);
+			}
+
+			private bool IsValidFor(Stack stack)
+			{
+				return true;
+			}
+		}
+	}
+}
