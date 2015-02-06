b8bdb5ca61aae2ee2a54c0271e6fe263beccd707
JamesBrill <james.brill@red-gate.com>
2015-02-06 17:06:59 +0000

    R#: Move Player to production code.

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using CelticEgyptianRatscrewKata.Rules;
+
+namespace CelticEgyptianRatscrewKata
+{
+    internal class Player
+    {
+        private Cards _cards;
+        private readonly ISnapRule _validSnap;
+
+        public Player(Cards cards, ISnapRule validSnap)
+        {
+            _cards = cards;
+            _validSnap = validSnap;
+        }
+
+        public Cards Hand
+        {
+            get
+            {
+                return _cards;
+            }
+        }
+
+        public bool HasWon()
+        {
+            return _cards.Count() == 52;
+        }
+
+        public void CallSnap(Cards stack)
+        {
+            if (_validSnap.IsValidFor(stack))
+            {
+                var cardList = _cards.ToList();
+                cardList.InsertRange(0, stack);
+                _cards = Cards.With(cardList.ToArray());
+            }
+        }
+
+        public void PlayCard(Cards stack)
+        {
+            stack.AddToTop(_cards.CardAt(0));
+            _cards.RemoveCardAt(0);
+        }
+    }
+}
--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/A_Player.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/A_Player.cs
@@ -100,45 +100,4 @@ namespace CelticEgyptianRatscrewKata.Tests
             return new Player(hand, snapRule);
         }
     }
-
-    internal class Player
-    {
-        private Cards _cards;
-        private readonly ISnapRule _validSnap;
-
-        public Player(Cards cards, ISnapRule validSnap)
-        {
-            _cards = cards;
-            _validSnap = validSnap;
-        }
-
-        public Cards Hand
-        {
-            get
-            {
-                return _cards;
-            }
-        }
-
-        public bool HasWon()
-        {
-            return _cards.Count() == 52;
-        }
-
-        public void CallSnap(Cards stack)
-        {
-            if (_validSnap.IsValidFor(stack))
-            {
-                var cardList = _cards.ToList();
-                cardList.InsertRange(0, stack);
-                _cards = Cards.With(cardList.ToArray());
-            }
-        }
-
-        public void PlayCard(Cards stack)
-        {
-            stack.AddToTop(_cards.CardAt(0));
-            _cards.RemoveCardAt(0);
-        }
-    }
 }
