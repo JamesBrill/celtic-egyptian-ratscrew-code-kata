256ba318f39d956c4283e2f9a912f59943b95a5c
JamesBrill <james.brill@red-gate.com>
2015-02-06 15:55:25 +0000

    First test: Player wins if they have all the cards.

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/PlayerFacts.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    [TestFixture]
+    class PlayerFacts
+    {
+        [Test]
+        public void Has_Won_If_They_Have_All_The_Cards()
+        {
+            var player = new Player(Enumerable.Repeat(new Card(Suit.Clubs, Rank.Ace), 52));
+            var hasWon = player.HasWon();
+
+            Assert.That(hasWon);
+        }
+    }
+
+    internal class Player
+    {
+        public Player(IEnumerable<Card> cards)
+        {
+            
+        }
+
+        public bool HasWon()
+        {
+            return true;
+        }
+    }
+}
