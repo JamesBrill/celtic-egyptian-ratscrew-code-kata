dfb3326c4a87e3c0ca1e03b506922b2436a84965
JamesBrill <james.brill@red-gate.com>
2015-02-06 16:27:06 +0000

    Third test: correctly calling stack results in the cards being added to the player's hand.

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/PlayerFacts.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/PlayerFacts.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using CelticEgyptianRatscrewKata.Rules;
+using Moq;
 using NUnit.Framework;
 
 namespace CelticEgyptianRatscrewKata.Tests
@@ -10,7 +12,10 @@ namespace CelticEgyptianRatscrewKata.Tests
         [Test]
         public void Has_Won_If_They_Have_All_The_Cards()
         {
-            var player = new Player(Enumerable.Repeat(new Card(Suit.Clubs, Rank.Ace), 52));
+            var player = new Player(
+                Cards.With(
+                    Enumerable.Repeat(new Card(Suit.Clubs, Rank.Ace), 52).ToArray()), 
+                Mock.Of<ISnapRule>());
             var hasWon = player.HasWon();
 
             Assert.That(hasWon);
@@ -19,26 +24,56 @@ namespace CelticEgyptianRatscrewKata.Tests
         [Test]
         public void Has_Not_Won_If_They_Do_Not_Have_All_The_Cards()
         {
-            var player = new Player(Cards.With(new Card(Suit.Clubs, Rank.Ace)));
+            var player = new Player(Cards.With(new Card(Suit.Clubs, Rank.Ace)), Mock.Of<ISnapRule>());
             
             var playerHasWon = player.HasWon();
 
             Assert.That(playerHasWon, Is.False);
         }
+
+        [Test]
+        public void If_Player_Correctly_Calls_Snap_Then_Stack_Is_Added_To_Bottom_Of_Their_Pile()
+        {
+            var stack = Cards.With(new Card(Suit.Spades, Rank.Queen));
+            var validSnap = new Mock<ISnapRule>();
+            validSnap.Setup(x => x.IsValidFor(stack)).Returns(true);
+
+            var player = new Player(Cards.Empty(), validSnap.Object);
+            player.CallSnap(stack);
+            var pile = player.Hand;
+            
+            CollectionAssert.AreEqual(stack, pile.Take(stack.Count()));
+        }
     }
 
     internal class Player
     {
-        private readonly IEnumerable<Card> _cards;
+        private Cards _cards;
 
-        public Player(IEnumerable<Card> cards)
+        public Player(Cards cards, ISnapRule validSnap)
         {
             _cards = cards;
+        }
+
+        public Cards Hand
+        {
+            get
+            {
+                return _cards;
+            }
         }
 
         public bool HasWon()
         {
             return _cards.Count() == 52;
+        }
+
+        public void CallSnap(Cards stack)
+        {
+            var cardList = _cards.ToList();
+            cardList.InsertRange(0, stack);
+
+            _cards = Cards.With(cardList.ToArray());
         }
     }
 }
